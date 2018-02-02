549595d60ac1245928a4ca2f98e96ca9d13405e0
php 09 <php.09@tom.cec.ime.usp.br>
2018-02-02 19:48:16 -0200

    Atualizando livros

--- resources/views/editora/livros.blade.php
+++ resources/views/editora/livros.blade.php
@@ -2,10 +2,48 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Livros</title>
+    <title>Dados</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    <h1>Teste</h1>
+    <div class="container">
+      <div class="page-header">
+        <h1>Listagem de editoras</h1>
+      </div>
 
+      <div class="row">
+        <div class="col-md-6">
+          <div class="panel panel-primary">
+            <div class="panel-heading">Editoras</div>
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Nome</th>
+                  <th>Email</th>
+                  <th>#</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                @foreach ($livros as $livro)
+                  <tr>
+                    <td>{{$livro->id}}</td>
+                    <td>{{$livro->titulo}}</td>
+                    <td>{{$livro->preco}}</td>
+                    <td>{{$livro->editora_id}}</td>
+                  </tr>
+                  @endforeach
+              </tbody>
+            </table>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+    <script type="text/javascript" src="/js/app.js">
+
+    </script>
   </body>
 </html>
